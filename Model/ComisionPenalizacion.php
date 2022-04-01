8259ee960bda00298b55bc990349a2d9759e323b
Carlos Garcia Gomez <neorazorx@gmail.com>
2022-04-01 18:30:17 +0200

    Corregido el namespace.

--- Model/ComisionPenalizacion.php
+++ Model/ComisionPenalizacion.php
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-namespace FacturaScripts\PLugins\Comisiones\Model;
+namespace FacturaScripts\Plugins\Comisiones\Model;
 
 use FacturaScripts\Core\Model\Base;
 use FacturaScripts\Dinamic\Model\Agente as DinAgente;

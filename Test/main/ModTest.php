142f5bc7d2beb1c4b80418e27c1c1927c9d9b2be
Daniel Fernández <daniel89fg@gmail.com>
2024-07-24 13:39:05 +0200

    Añadido test para los Mod

---
+++ Test/main/ModTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is part of Comisiones plugin for FacturaScripts.
+ * Copyright (C) 2024 Carlos Garcia Gomez <carlos@facturascripts.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace FacturaScripts\Test\Plugins;
+
+use FacturaScripts\Plugins\Comisiones\Mod\CalculatorMod;
+use FacturaScripts\Plugins\Comisiones\Mod\SalesFooterHTMLMod;
+use FacturaScripts\Plugins\Comisiones\Mod\SalesLineHTMLMod;
+use FacturaScripts\Test\Traits\LogErrorsTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Daniel Fernández Giménez <hola@danielfg.es>
+ */
+final class ModTest extends TestCase
+{
+    use LogErrorsTrait;
+
+    public function testCalculatorMod()
+    {
+        new CalculatorMod();
+    }
+
+    public function testSalesFooterHTMLMod()
+    {
+        new SalesFooterHTMLMod();
+    }
+
+    public function testSalesLineHTMLMod()
+    {
+        new SalesLineHTMLMod();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->logErrors();
+    }
+}
